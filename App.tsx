f9014dc11919662c86ea3110160b474f86b0d5a5
Vladimir-Ciuculescu <vladimir.ciuculescu@gmail.com>
2023-06-10 20:33:11 +0300

    Added react navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, StyleSheet } from "react-native";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text className="text-red-500 text-3xl">Weather app</Text>
       <StatusBar style="auto" />
     </View>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
